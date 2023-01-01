1e6c822d9cba553cafa6dd94f996a8a30733a5dd
Rupak Tyagi <rupaktyagi21@gmail.com>
2023-01-01 22:17:03 +0530

    Add files via upload

---
+++ map/src/map/A.java
@@ -0,0 +1,25 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class A {
+
+	public static void  main(String[] args)
+	{
+		Map<Integer, String>map=new HashMap<>();
+		
+		map.put(1, "Rupak");
+		System.out.println(map+"   *****");
+		map.put(2, "Tushar");
+		System.out.println(map+"   *****");
+		map.put(1, "Rupak Tyagi");
+		System.out.println(map+"   *****");
+		
+		
+		
+		
+		
+	}
+	
+}
